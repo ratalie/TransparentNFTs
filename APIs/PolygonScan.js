d398a4aa77c2dbb49e66142ceb4af036055a4c7f
Natalie Gil <natalieg@outlook.com>
2022-01-16 07:18:19 -0500

    Create PolygonScan.js

---
+++ APIs/PolygonScan.js
@@ -0,0 +1,11 @@
+/* 
+FOR ERC1155 
+*/
+https://api.polygonscan.com/api
+   ?module=token
+   &action=tokeninfo
+   &contractaddress="MATICaddress"
+   &apikey=YourApiKeyToken
+/* 
+read OVERVIEW field as per standard
+*/
