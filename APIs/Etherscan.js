fbd7acb1defe375a3941490b7186ca3d12b9821c
Natalie Gil <natalieg@outlook.com>
2022-01-16 07:13:36 -0500

    Create Etherscan.js

---
+++ APIs/Etherscan.js
@@ -0,0 +1,7 @@
+/* FOR ERC20 and ERC721 */
+https://api.etherscan.io/api
+   ?module=token
+   &action=tokeninfo
+   &contractaddress="ethaddress"
+   &apikey=YourApiKeyToken 
+/* read OVERVIEW field as per standard */
