133fa9c71b9c31da189663d6d16367aa463216a5
Natalie Gil <natalieg@outlook.com>
2022-01-16 07:17:33 -0500

    Update Etherscan.js

--- APIs/Etherscan.js
+++ APIs/Etherscan.js
@@ -1,7 +1,11 @@
-/* FOR ERC20 and ERC721 */
+/* 
+FOR ERC20 and ERC721
+*/
 https://api.etherscan.io/api
    ?module=token
    &action=tokeninfo
    &contractaddress="ETHaddress"
    &apikey=YourApiKeyToken 
-/* read OVERVIEW field as per standard */
+/* 
+read OVERVIEW field as per standard
+*/
