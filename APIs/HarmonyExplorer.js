7d6c88d77d586d112216b64cb4cfe7a3454b8d88
Natalie Gil <natalieg@outlook.com>
2022-01-16 07:36:35 -0500

    Create HarmonyExplorer.js

---
+++ APIs/HarmonyExplorer.js
@@ -0,0 +1,7 @@
+/* FOR HRC20, HRC721, HRC1155*/
+https://explorer-v2-api.hmny.io/
+   ?module=token
+   &action=tokeninfo
+   &contractaddress="ONEaddress"
+   &apikey=YourApiKeyToken 
+/* read OVERVIEW field as per standard */
