a6b64d4197916c1e4c8e2d37345f06214b71ac53
Natalie Gil <natalieg@outlook.com>
2022-02-06 17:56:49 -0500

    Update NFTPortApi.js

--- NFTPort/NFTPortApi.js
+++ NFTPort/NFTPortApi.js
@@ -1,8 +1,14 @@
-fetch("https://api.nftport.xyz/v0/nfts/contract_address/token_id?chain=ethereum", {
+/* 
+FOR 
+   &chain=Ethereum, Polygon all supported by NFT Port
+   &apikey=YourApiKeyToken 
+read OVERVIEW/Or related field as per standard defined by TransparentNFTs to store inclusive data
+*/
+fetch("https://api.nftport.xyz/v0/nfts/contract_address/token_id?chain=thechain", {
   "method": "GET",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Your API key"
+    "Authorization": YourAPIkeyToken
   }
 })
 .then(response => {
