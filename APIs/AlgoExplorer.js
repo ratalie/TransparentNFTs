38ea0a3ff4168d254dde0fa2df97a9138c89207b
Natalie Gil <natalieg@outlook.com>
2022-04-06 21:32:45 -0500

    Update AlgoExplorer.js

--- APIs/AlgoExplorer.js
+++ APIs/AlgoExplorer.js
@@ -1,4 +1,4 @@
-// Function used to print created asset for account and assetid ASA
+// Function used to print created asset for account and assetid ASA-
 // Extract description field proposed use as standard to store info about asser
 const printCreatedAsset = async function (algodclient, account, assetid) {
     // note: if you have an indexer instance available it is easier to just search accounts for an asset
