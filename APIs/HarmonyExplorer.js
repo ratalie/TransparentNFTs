14a881e6c1fefbe03c5abcc3cedb754216121633
Natalie Gil <natalieg@outlook.com>
2022-04-06 21:33:24 -0500

    Update HarmonyExplorer.js

--- APIs/HarmonyExplorer.js
+++ APIs/HarmonyExplorer.js
@@ -1,10 +1,12 @@
-/* FOR HRC20, HRC721, HRC1155
+/* 
+FOR HRC20, HRC721, HRC1155
 https://explorer-v2-api.hmny.io/
    ?module=token
    &action=tokeninfo
    &contractaddress="ONEaddress"
    &apikey=YourApiKeyToken 
-read OVERVIEW field as per standard */
+read OVERVIEW field as per standard 
+*/
     var Web3 = require('web3');
     var web3 = new Web3(new Web3.providers.HttpProvider());
     var version = web3.version.api;
