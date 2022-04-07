5d94c5b6ff7014be4baac556b537f0f5474673cb
Natalie Gil <natalieg@outlook.com>
2022-04-06 21:33:44 -0500

    Update PolygonScan.js

--- APIs/PolygonScan.js
+++ APIs/PolygonScan.js
@@ -1,10 +1,12 @@
-/* FOR ERC1155 
+/* 
+FOR ERC1155 
 https://api.polygonscan.com/api
    ?module=token
    &action=tokeninfo
    &contractaddress="MATICaddress"
    &apikey=YourApiKeyToken
-read OVERVIEW field as per standard */
+read OVERVIEW field as per standard 
+*/
 var Web3 = require('web3');
     var web3 = new Web3(new Web3.providers.HttpProvider());
     var version = web3.version.api;
